b43ba1b5473bb07a1ad7d0892e1a426061ab01c2
Saglara <nohaevass@yandex.ru>
2022-12-17 11:14:55 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -5,3 +5,13 @@
 # triangle(1, 1, 2)
 # Это не треугольник
 # triangle(7, 6, 10)
+
+def Triangle(a, b, c):
+    if a+b>c and a+c>b and b+c>a:
+        return True
+    else:
+        return False
+a = int(input('введите а: '))
+b = int(input('введите b: '))
+c = int(input('введите c: '))
+print('Это треугольник' if Triangle(a, b, c) else "Это не треугольник")
