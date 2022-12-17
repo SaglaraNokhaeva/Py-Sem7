893d5f97d02eedaf99764e17d3fb38840a398eec
Saglara <nohaevass@yandex.ru>
2022-12-17 10:22:48 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,15 +1,16 @@
 # 1.напечатать сторку в одну линию - C:\WINDOWS\System32\drivers\etc\nst
-print("C:\WINDOWS\System32\drivers\etc\\nst")
-print(r"C:\WINDOWS\System32\drivers\etc\nst")
+# print("C:\WINDOWS\System32\drivers\etc\\nst")
+# print(r"C:\WINDOWS\System32\drivers\etc\nst")
 
 
 
 
 
 
+#2. Сортировка:
+#  a = [4, 3, -10, 1, 7, 12]
+# a=[4, -10, 12, 3, 1, 7]
 
-
-
-
-# Сергей Сердюк: a = [4, 3, -10, 1, 7, 12]
-# Сергей Сердюк: 2) a = [4, 3, -10, 1, 7, 12]a=[4, -10, 12, 3, 1, 7]
+a = [4, 3, -10, 1, 7, 12]
+a.sort(key=lambda x: x%2)
+print(a)
