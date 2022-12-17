0767c89fe915dced2206a9eda381d20e136458a8
Saglara <nohaevass@yandex.ru>
2022-12-17 10:59:06 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -10,3 +10,7 @@ string="Лена Енисей Волга Дон"
 river=string.split()
 river_res=sorted(river, key=lambda x: len(x), reverse=True)
 print(' '.join(river_res))
+
+# или
+# s=sorted(input().split(), key=lambda x: len(x))[::-1]
+# print(*s)
